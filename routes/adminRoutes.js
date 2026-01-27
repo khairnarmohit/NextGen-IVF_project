7d886418247d64e9f8c132488aa6af916f2036cb
aresakshi <aresadhana04@gmail.com>
2026-01-27 19:05:33 +0530

    pateint -review page
    pateinet -review page

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -15,4 +15,56 @@ router.get("/patient-review", adminController.getPatientReviewPage);
 
 router.get("/gallery", adminController.getGalleryPage);
 
+// privacy page
+router.get("/privacy", adminController.getPrivacyPage);
+
+
+// pateint review
+router.get("/patient-review", adminController.getPatientReviewPage);
+
+// router.get("/patient-review/edit/:id", adminController.getEditPatientReviewPage); 
+// -----------------------------------------------------
+
+router.post("/patient-review/save", adminController.saveReview);
+router.post("/patient-review/update", adminController.updateReview);
+router.get("/patient-review/delete/:id", adminController.deleteReview);
+
+
+// privacy
+router.get("/privacy", adminController.getPrivacyPage);
+router.post("/privacy/save", adminController.savePrivacy);
+router.post("/privacy/update", adminController.updatePrivacy);
+router.get("/privacy/delete/:id", adminController.deletePrivacy);
+
+
+
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
