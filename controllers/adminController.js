9cbe09290f9bad3bd05de9488eb22e125935f4e3
Lalit Magar <lalitmagar1729@gmail.com>
2026-02-04 11:28:59 +0530

    Login Issue
    Login Issue

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -31,6 +31,7 @@ exports.postLogin = async (req, res) => {
         .status(401)
         .render("admin/login", { message: "Invalid Email or Password" });
     }
+    console.log(req.body.email, req.body.password);
   } catch (error) {
     console.error(error);
     res.status(500).render("error", { message: "Login Page Error" });
