08dc488eb1c0a4bd5902d28799762bb1e601c51e
khairnarmohit <102132224+khairnarmohit@users.noreply.github.com>
2026-02-19 23:21:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use((req, res, next) => {
   res.status(404).render("404");
 });
 
-app.listen(3001, function () {
+app.listen(3003, function () {
   console.log("Server is running on http://localhost:3000");
 });
